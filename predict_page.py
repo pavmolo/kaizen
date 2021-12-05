d47bc056d1abed1366cc65907831745963ba8748
pavmolo <74321783+pavmolo@users.noreply.github.com>
2021-12-05 16:58:19 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -35,15 +35,15 @@ def show_predict_page():
 
     industry = st.selectbox("Ваша отрасль:", industry_list)
     market_state = st.selectbox("Охарактеризуйте состояние сектора, в котором вы работаете:", gro_state_list)
-    revenue = st.number_input("Какова ваша выручка, млн, руб. в год:", value='int')
+    revenue = st.number_input("Какова ваша выручка, млн, руб. в год:", value=0)
     margin = st.number_input("Какова ваша маржа операционной прибыли, % к выручке:")
     growth = st.slider("Каков ваш среднегодовой рост выручки в % за последние 3 года", -20, 100, 2)
 
     ok = st.button("Определить прибыль")
     if ok:
         lost = lost_profit(industry, market_state, revenue, margin, growth)
-        st.subheader(f"Предварительная оценка разницы в прибыли при сравнении с компаниями мирового класса: {lost[0]:.2f} млн. руб.")
+        st.subheader(f"Предварительная оценка разницы в прибыли при сравнении с компаниями мирового класса: {lost[0]:.0f} млн. руб.")
         st.subheader(f"в том числе:")
-        st.subheader(f"Прибыль упущенная в операционной деятельности: {lost[1]:.2f} млн.руб.")
-        st.subheader(f"Прибыль упущенная из-за отсутствия роста: {lost[2]:.2f} млн.руб.")
+        st.subheader(f"Прибыль упущенная в операционной деятельности: {lost[1]:.0f} млн.руб.")
+        st.subheader(f"Прибыль упущенная из-за отсутствия роста: {lost[2]:.0f} млн.руб.")
 show_predict_page()

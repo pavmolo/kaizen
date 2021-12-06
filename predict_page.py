5d8f9cefac6f4fa520e060a2c908bf3e6be7bb76
pavmolo <74321783+pavmolo@users.noreply.github.com>
2021-12-06 21:17:37 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -72,7 +72,7 @@ def break_down_g(a_12, a_13, a_14, a_15, a_16, a_17):
 def show_predict_page():
     #image = Image.open('https://www.kaizen.com/images/kaizen_logo.png')
     #st.image(image, caption='Kaizen Institute')
-    st.markdown('''<a href="http://ru.kaizen.com/"><img src='https://www.kaizen.com/images/kaizen_logo.png' style="width: 50%; margin-left: auto; margin-right: auto;"></a><p>''', unsafe_allow_html=True)
+    st.markdown('''<a href="http://ru.kaizen.com/"><img src='https://www.kaizen.com/images/kaizen_logo.png' style="width: 50%; margin-left: 25%; margin-right: 25%; text-align: center;"></a><p>''', unsafe_allow_html=True)
     st.title("Определи свой потенциал")
     st.subheader('Нам необходима информация, чтобы спрогнозировать ваши показатели прибыли')
 

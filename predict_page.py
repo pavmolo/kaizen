58c03796662e9937a5a028dc29c837b5c91434a6
pavmolo <74321783+pavmolo@users.noreply.github.com>
2021-12-06 20:44:42 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -129,9 +129,13 @@ def show_predict_page():
     fig_3 = px.bar(lost_growth, x=0, y=lost_growth.index)
     fig_2.update_layout(title = "Разбивка операционной дельты", width=900, xaxis_title="млн. руб. упущенной прибыли", yaxis_title="Операционные факторы Kaizen")
     fig_3.update_layout(title = "Разбивка дельты роста", width=900, xaxis_title="млн. руб. упущенной прибыли", yaxis_title="Факторы Роста Kaizen")
-    if len(lost_oper) != 0:
+    if lost_oper.isnull():
+        print('Нет данных')
+    else:
         st.plotly_chart(fig_2, use_container_width=False, sharing="streamlit")
-    if len(lost_growth) != 0:
+    if lost_growth.isnull():
+        print('Нет данных')
+    else:
         st.plotly_chart(fig_3, use_container_width=False, sharing="streamlit")
 
 # Вызываем приложение

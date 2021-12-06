fa4eb66d6ecceaf67a9d207a822c2683ac6caf8b
pavmolo <74321783+pavmolo@users.noreply.github.com>
2021-12-06 14:57:47 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -52,23 +52,7 @@ def lost_breaksown(a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_10, a_11):
     
 
     
-def break():
-    anw_0 = st.radio(df_deltas_breakdown.index[0], answers_list, index=0)
-    anw_1 = st.radio(df_deltas_breakdown.index[1], answers_list, index=0)
-    anw_2 = st.radio(df_deltas_breakdown.index[2], answers_list, index=0)
-    anw_3 = st.radio(df_deltas_breakdown.index[3], answers_list, index=0)
-    anw_4 = st.radio(df_deltas_breakdown.index[4], answers_list, index=0)
-    anw_5 = st.radio(df_deltas_breakdown.index[5], answers_list, index=0)
-    anw_6 = st.radio(df_deltas_breakdown.index[6], answers_list, index=0)
-    anw_7 = st.radio(df_deltas_breakdown.index[7], answers_list, index=0)
-    anw_8 = st.radio(df_deltas_breakdown.index[8], answers_list, index=0)
-    anw_9 = st.radio(df_deltas_breakdown.index[9], answers_list, index=0)
-    anw_10 = st.radio(df_deltas_breakdown.index[10], answers_list, index=0)
-    ok = st.button("Разбить дельту")
-    if ok:
-        st.title("Результат")
-        lost_raz = lost_breaksown(anw_0, anw_1, anw_2, anw_3, anw_4, anw_5, anw_6, anw_7, anw_8, anw_9, anw_10)
-        st.title(lost_raz)
+    
     
     
 
@@ -100,7 +84,22 @@ def show_predict_page():
             return fig
         graph = grafik()
         st.plotly_chart(graph, use_container_width=False, sharing="streamlit")
-        break()
+        anw_0 = st.radio(df_deltas_breakdown.index[0], answers_list, index=0)
+        anw_1 = st.radio(df_deltas_breakdown.index[1], answers_list, index=0)
+        anw_2 = st.radio(df_deltas_breakdown.index[2], answers_list, index=0)
+        anw_3 = st.radio(df_deltas_breakdown.index[3], answers_list, index=0)
+        anw_4 = st.radio(df_deltas_breakdown.index[4], answers_list, index=0)
+        anw_5 = st.radio(df_deltas_breakdown.index[5], answers_list, index=0)
+        anw_6 = st.radio(df_deltas_breakdown.index[6], answers_list, index=0)
+        anw_7 = st.radio(df_deltas_breakdown.index[7], answers_list, index=0)
+        anw_8 = st.radio(df_deltas_breakdown.index[8], answers_list, index=0)
+        anw_9 = st.radio(df_deltas_breakdown.index[9], answers_list, index=0)
+        anw_10 = st.radio(df_deltas_breakdown.index[10], answers_list, index=0)
+        ok = st.button("Разбить дельту")
+        if ok:
+            st.title("Результат")
+            lost_raz = lost_breaksown(anw_0, anw_1, anw_2, anw_3, anw_4, anw_5, anw_6, anw_7, anw_8, anw_9, anw_10)
+            st.title(lost_raz)
 
 
 # Вызываем приложение

ad6f5505c995232b674429d0a894a22456161b13
pavmolo <74321783+pavmolo@users.noreply.github.com>
2021-12-06 15:50:17 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -74,9 +74,17 @@ def show_predict_page():
     graph = grafik()
     st.plotly_chart(graph, use_container_width=False, sharing="streamlit")
     st.title("Оцените следующие аспекты вашей компании:")
-    anw_0 = st.radio(df_deltas_breakdown.index[0], list(answers_list), index=0)
-    st.markdown(anw_0)
-        
+    anw_0 = st.radio(df_deltas_breakdown.index[0], answers_list, index=0)
+    anw_1 = st.radio(df_deltas_breakdown.index[1], answers_list, index=0)
+    anw_2 = st.radio(df_deltas_breakdown.index[2], answers_list, index=0)
+    anw_3 = st.radio(df_deltas_breakdown.index[3], answers_list, index=0)
+    anw_4 = st.radio(df_deltas_breakdown.index[4], answers_list, index=0)
+    anw_5 = st.radio(df_deltas_breakdown.index[5], answers_list, index=0)
+    anw_6 = st.radio(df_deltas_breakdown.index[6], answers_list, index=0)
+    anw_7 = st.radio(df_deltas_breakdown.index[7], answers_list, index=0)
+    anw_8 = st.radio(df_deltas_breakdown.index[8], answers_list, index=0)
+    anw_9 = st.radio(df_deltas_breakdown.index[9], answers_list, index=0)
+    anw_10 = st.radio(df_deltas_breakdown.index[10], answers_list, index=0)
 
 
 # Вызываем приложение

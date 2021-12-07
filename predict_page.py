af1f72634762afb260e217ac399f1ee36942d636
pavmolo <74321783+pavmolo@users.noreply.github.com>
2021-12-07 15:45:27 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -141,9 +141,9 @@ def show_predict_page():
 
         lost_total = lost_oper_fin.append(lost_growth_fin)        
 
-        fig_1 = px.bar(lost_total, y='Оценка', x='Аспект', text='Оценка', color='Направление', showlegend=False)
+        fig_1 = px.bar(lost_total, y='Оценка', x='Аспект', text='Оценка', color='Направление')
         fig_1.update_traces(texttemplate='%{text:.2s}', textposition='auto')
-        fig_1.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', title = "Разбивка упущенной прибыли", yaxis_title="млн. руб. упущенной прибыли", xaxis_title="Факторы Kaizen")
+        fig_1.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', title = "Разбивка упущенной прибыли", yaxis_title="млн. руб. упущенной прибыли", xaxis_title="Факторы Kaizen", showlegend=False)
         st.plotly_chart(fig_1, use_container_width=True, sharing="streamlit")        
 
 #        if len(lost_oper) != 0:

3b62d462bc0d1037577a5b7503cd4219e94c97f7
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-06-17 05:55:11 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -5,16 +5,39 @@ import plotly.graph_objects as go
 import plotly.express as px
 #from PIL import Image
 
-gsheetid = '1S7gJojFKedjSvSRM9npIDAzN_6mkSZhgEdGpNbxXnK0'
-list_1 = 'sector_margin'
-list_2 = 'growth_rate'
-list_3 = 'deltas_breakdown'
-list_4 = 'answer_score'
 
-df_sector_margin_csv = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, list_1)
-df_growth_rate_csv = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, list_2)
-deltas_breakdown_csv = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, list_3)
-answer_score_csv = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, list_4)
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+#credentials = service_account.Credentials.from_service_account_file(
+#        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=SCOPES)
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1etUSlGdjQruAn5AjPCat2R-k9LhXnapnXm4szTZJ3Pg'
+service = build('sheets', 'v4', credentials=credentials).spreadsheets().values()
+def df_maker(sheet, columns):
+  resp = service.get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=sheet).execute()
+  values = resp.get('values', [])
+  df = pd.DataFrame(values, columns=columns)
+  return df
+
+df_sector_margin = df_maker('sector_margin', ['sector', 'margin'])
+df_growth_rate = df_maker('growth_rate',['growth_state', 'growth_rate'])
+df_deltas_breakdown = df_maker('deltas_breakdown',['answer', 'question_score'])
+df_answer_score = df_maker('answer_score',['answer_id', 'answer', 'answer_score'])
+
+
+
+df_growth_rate.set_index('growth_state', inplace=True)
+df_sector_margin.set_index('sector', inplace=True)
+df_deltas_breakdown.set_index('answer', inplace=True)
+df_answer_score.set_index('answer_id', inplace=True)
+
+df_sector_margin['margin'] = pd.to_numeric(df_sector_margin['margin'])
+df_growth_rate['growth_rate'] = pd.to_numeric(df_growth_rate['growth_rate'])
+df_deltas_breakdown['question_score'] = pd.to_numeric(df_deltas_breakdown['question_score'])
+df_answer_score['answer_score'] = pd.to_numeric(df_answer_score['answer_score'])
+#df_answer_score['answer_id'] = pd.to_numeric(df_answer_score['answer_id'])
 
 
 df_sector_margin = pd.read_csv(df_sector_margin_csv)
